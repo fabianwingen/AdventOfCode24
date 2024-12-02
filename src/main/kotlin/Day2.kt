a24a88f0883b6cd296a49e2da4a684ef5b465696
Fabian Wingen <fabian.wingen@hhu.de>
2024-12-02 22:36:41 +0100

    add Day2

---
+++ src/main/kotlin/Day2.kt
@@ -0,0 +1,49 @@
+import java.io.File
+import kotlin.math.abs
+
+val conditionDifference: (Pair<Int, Int>) -> Boolean = { abs(it.first - it.second) <= 3 }
+val conditionIncreasing: (Pair<Int, Int>) -> Boolean = { it.first < it.second }
+val conditionDecreasing: (Pair<Int, Int>) -> Boolean = { it.first > it.second }
+
+fun main() {
+    val path = "src/main/resources/input2.txt"
+    val file = File(path)
+    val parsedInput = parseInputs(file)
+    println(parsedInput)
+    println(part1(parsedInput))
+    println(part2(parsedInput))
+}
+
+private fun part1(inputs: List<List<Int>>): Int {
+    return getValidLines(inputs).size
+}
+private fun part2(inputs: List<List<Int>>): Int {
+    val validLines = getValidLines(inputs)
+    val newValidLines = inputs.filterNot { validLines.contains(it) }.filter { list -> checkIfValid(list) }
+
+    return validLines.size + newValidLines.size
+}
+
+private fun getValidLines(inputs: List<List<Int>>): List<List<Int>> {
+    val validLines = inputs.filter {
+        val pairs = it.zipWithNext()
+        pairs.all { pair -> conditionDifference(pair) && conditionIncreasing(pair) } ||
+                pairs.all { pair -> conditionDifference(pair) && conditionDecreasing(pair) }
+    }
+    return validLines
+}
+
+fun checkIfValid(list: List<Int>): Boolean { //TODO: fix, brute force
+    for (i in list.indices) {  val newList = list.filterIndexed {index, _ -> index != i}
+        if (getValidLines(listOf(newList)).isNotEmpty()) {
+            return true
+        }
+    }
+    return false
+}
+
+private fun parseInputs(file: File): List<List<Int>> {
+    val list = file.readLines().map { it.split(" ").map { el -> el.toInt() } }
+    return list
+}
+
