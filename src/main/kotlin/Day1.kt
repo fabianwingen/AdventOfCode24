ab66cbf7cbddc5214292092016a60631ced2f549
Fabian Wingen <fabian.wingen@hhu.de>
2024-12-02 22:38:41 +0100

    fix day1 method visibility

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -9,18 +9,18 @@ fun main() {
     println(part2(parsedInput))
 }
 
-fun part1(inputs: List<List<Int>>): Int {
+private fun part1(inputs: List<List<Int>>): Int {
     val list1Sorted = inputs[0].sorted()
     val list2Sorted = inputs[1].sorted()
     return list1Sorted.mapIndexed { index, el -> abs(el - list2Sorted[index]) }.sum()
 }
 
-fun part2(inputs: List<List<Int>>): Int {
+private fun part2(inputs: List<List<Int>>): Int {
     val groupedBy = inputs[1].groupingBy { it }.eachCount()
     return inputs[0].sumOf { it * groupedBy.getOrDefault(it, 0) }
 }
 
-fun parseInputs(file: File): List<List<Int>> {
+private fun parseInputs(file: File): List<List<Int>> {
     val (list1, list2) = file.readLines().map { it.split("   ") }
         .map { it[0].toInt() to it[1].toInt() }
         .unzip()
